124074877f3a35305dde20382ad09e82c8c8d556
merthacioglu <mhacioglu96@gmail.com>
2023-11-16 22:31:59 +0000

    navbar completed

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,67 @@
-import {Component, ElementRef, Renderer2, ViewChild} from '@angular/core';
-
-class Renderer {
-}
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  QueryList,
+  Renderer2,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
-
-
 export class HeaderComponent {
-  @ViewChild('dropdown', {static: true}) dd!: ElementRef;
+
+  @ViewChildren('dropdowns') dropdowns: QueryList<ElementRef>;
+
+  navbarElements = {
+    'HOME': null,
+    'CORPORATE': {
+      elements: ['ABOUT US', 'CAREER'],
+      index: 0
+    },
+    'PRODUCTS': {
+      elements: ['RACK CLAD BUILDING SYSTEMS', 'TEST'],
+      index: 1
+    },
+    'REFERENCES': null,
+    'OUR PROJECTS': {
+      elements: ['1', '2'],
+      index: 2
+    },
+    'CATALOGS': null,
+    'VIDEO': null,
+    'CONTACT': null
+  };
 
   constructor(private r2: Renderer2) {
-  }
-  onMouseEnter() {
-    this.r2.removeClass(this.dd.nativeElement, 'hidden');
+
   }
 
-  onMouseLeave() {
-    setTimeout(() => {
-      console.log('added');
-      this.r2.addClass(this.dd.nativeElement, 'hidden');
-    }, 1000);
+
+
+  onHoverNavElement(key: string): void {
+    if (this.navbarElements[key] !== null){
+      const index = this.navbarElements[key].index;
+      this.r2.removeClass(this.dropdowns.toArray().at(index).nativeElement, 'hidden');
+    }
+
+
   }
+
+  onLeaveNavElement(): void {
+
+    this.dropdowns.forEach(dropdownEl => {
+      this.r2.addClass(dropdownEl.nativeElement, 'hidden');
+    })
+
+  }
+
+  returnZero() {
+    return 0
+  }
+
 }
